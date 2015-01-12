92f41251504b6c83d749a216d2dd189b59591df2
Marco Pivetta <ocramius@gmail.com>
2015-01-12 18:12:03 +0000

    Importing state as of zendframework/zf2@a9a025a39645543f50279e05713dd42de9d8bcc2 (1421086323)
    Automatic import via rsync

--- Server.php
+++ Server.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -557,13 +557,10 @@ class Server implements ZendServerServer
                     throw new Exception\InvalidArgumentException('One or more invalid functions specified in array');
                 }
             }
-
         } elseif (is_string($function) && function_exists($function)) {
             $this->functions[] = $function;
-
         } elseif ($function == SOAP_FUNCTIONS_ALL) {
             $this->functions = SOAP_FUNCTIONS_ALL;
-
         } else {
             throw new Exception\InvalidArgumentException('Invalid function specified');
         }
@@ -730,13 +727,10 @@ class Server implements ZendServerServer
 
         if ($request instanceof DOMDocument) {
             $xml = $request->saveXML();
-
         } elseif ($request instanceof DOMNode) {
             $xml = $request->ownerDocument->saveXML();
-
         } elseif ($request instanceof SimpleXMLElement) {
             $xml = $request->asXML();
-
         } elseif (is_object($request) || is_string($request)) {
             if (is_object($request)) {
                 $xml = $request->__toString();
@@ -982,7 +976,6 @@ class Server implements ZendServerServer
             foreach ($class as $row) {
                 $this->registerFaultException($row);
             }
-
         } elseif (is_string($class) && class_exists($class) && (is_subclass_of($class, 'Exception') || 'Exception' === $class)) {
             $ref = new ReflectionClass($class);
 
